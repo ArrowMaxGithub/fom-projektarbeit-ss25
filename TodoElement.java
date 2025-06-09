92377beebb5affb73c844bf8d9cd38736668ae40
domik258 <dominikmik@gmx.de>
2025-06-09 21:08:23 +0200

    Neu nach dem Meeting heute.

--- TodoElement.java
+++ TodoElement.java
@@ -1,17 +1,18 @@
+//Initialisierung des Inhaltes eines Todo Elementes
 public final class TodoElement{
-    private final String header;
-    private final String message;
-
+    private final String header;                                                                                        //Überschrift des Todo Elementes
+    private final String message;                                                                                       //Inhalt des Todo Elementes
+    // Konstruktor zum Intialisierung des Todo Elementes mit Überschrift und Inahlt
     public TodoElement(String header, String message) {
         this.header = header;
         this.message = message;
     }
 
     public String getHeader() {
-        return header;
+        return header;																									//Rückgabe der Überschrift des Todo Elementes, wodurch diese zur Verfügung gestellt wird
     }
 
     public String getMessage() {
-        return message;
+        return message;																									//Rückgabe des Inhaltes des Todo Elementes, wodurch dieser zur Verfügung gestellt wird
     }
 }
