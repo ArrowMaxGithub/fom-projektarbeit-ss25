08bc7cada7136a8589853d80e24069f9f12a4aa3
Maximillian Pfeil <pfeil.maximillian@gmail.com>
2025-07-18 10:08:13 +0200

    Formatierung vereinheitlichen

--- TodoListModel.java
+++ TodoListModel.java
@@ -1,59 +1,66 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.AbstractListModel;
-//Klasse zur Verwaltung der Todo Liste und umordnen der Todo-Elemente
-public final class TodoListModel extends AbstractListModel<TodoElement>{
-    final ArrayList<TodoElement> list;                                                                                  //Speichern der Todo-Elemente in einer internen Liste
 
+// Klasse zur Verwaltung der Daten der To-do Liste (Modell in MVC)
+public final class TodoListModel extends AbstractListModel<TodoElement>{
+    final ArrayList<TodoElement> list; // To-do Elemente werden in einer privaten ArrayList gespeichert
+
+    // Konstruktor zur Initialisierung der Daten
     public TodoListModel() {
-        this.list = new ArrayList<>();																					//Konstruktor zur Initialisierung der internen Liste
+        this.list = new ArrayList<>(); 
     }
 
     @Override
     public int getSize() {
-        return this.list.size();																						//Gibt die Anzahl der Elemente in der Todo Liste zurück
+        return this.list.size(); // Gibt die Anzahl der Elemente in der To-do Liste zurück
     }
 
     @Override
     public TodoElement getElementAt(int index) {
-        return this.list.get(index);																					//Rückgabe des Index eines Todo Listen Elementes
+        return this.list.get(index); // Rückgabe des To-do Elements an Stelle index
     }
 
-    public Iterator<TodoElement> iterator(){																			//Iterator der ermöglicht die Elemente der Todo Liste zu durchlaufen
+    // Iterator zum Durchlaufen aller To-do Elemente der Liste
+    public Iterator<TodoElement> iterator(){
         return this.list.iterator();
     }
-    // Methode zum hinzufügen eines neuen Elementes in die Todo Liste
+
+    // Methode zum Hinzufügen eines neuen Elementes in die To-do Liste
     public void add(TodoElement element){
         int index = this.list.size();
         this.list.add(element);
         this.fireIntervalAdded(this, index, index);
     }
-    //Methode zum entfernen eines Elementes aus der Todo Liste
+
+    // Methode zum Entfernen eines Elementes aus der To-do Liste
     public void remove(TodoElement element){
         int index = this.list.indexOf(element);
         this.list.remove(element);
         this.fireIntervalRemoved(this, index, index);
     }
-    //Verschieben des Elementes nach oben, wenn es nicht an oberster Stelle der Todo Liste steht
+
+    // Verschieben des Elementes um eine Position nach oben
     public void moveElementUp(TodoElement element){
-        int index = this.list.indexOf(element);                                                                         //Abfrage des Index des Elementes
-        if (index == 0){                                                                                                //Überprüfung, ob das Element an oberster Stelle steht
+        int index = this.list.indexOf(element); // Abfrage des Index des Elementes
+        if (index == 0){ // Überprüfung, ob das Element bereits an oberster Stelle steht
             return;
         }
-        //Austausch des Elements mit dem darüber liegenden
+        // Austausch des Elements mit dem darüber liegenden mithilfe einer Hilfsvariable
         TodoElement swapped = this.list.set(index - 1, element);
         this.list.set(index, swapped);
-        this.fireContentsChanged(this, index - 1, index);                                                               //Benachrichtigung der GUI über den Platztausch
+        this.fireContentsChanged(this, index - 1, index); // Benachrichtigung aller Listener über den Platztausch
     }
-    //Verschieben eines Elementes nach unten, wenn es nicht an unterster Stelle der Todo Liste steht
+
+    // Verschieben des Elementes um eine Position nach unten
     public void moveElementDown(TodoElement element){
-        int index = this.list.indexOf(element);                                                                         //Abfrage des Index des Elementes
-        if (index == this.list.size() - 1){                                                                             //Überprüfung, ob das Element an letzter Stelle steht
+        int index = this.list.indexOf(element); // Abfrage des Index des Elementes
+        if (index == this.list.size() - 1){ // Überprüfung, ob das Element bereits an unterster Stelle steht
             return;
         }
-        //Austausch des Elementes mit dem darunter liegenden
+        // Austausch des Elementes mit dem darunter liegenden mithilfe einer Hilfsvariable
         TodoElement swapped = this.list.set(index + 1, element);
         this.list.set(index, swapped);
-        this.fireContentsChanged(this, index, index + 1);                                                               //Benachrichtigung der GUI über den Platztausch
+        this.fireContentsChanged(this, index, index + 1); // Benachrichtigung aller Listener über den Platztausch
     }
 }
--- TodoListView.java
+++ TodoListView.java
@@ -13,125 +13,142 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
-//Aufbau der grafischen Oberfläche
-public final class TodoListView implements ListDataListener{                                                            //Reaktion auf Änderungen im Modell durch den implementierung des DataListeners
-    private final TodoList controller;                                                                                  //Referenz auf die Hauptklasse TodoList
-    private final TodoListModel model;                                                                                  //Referenz auf Klasse TodoListModel
-    private final JPanel masterPanel;                                                                                   //Top Level Komponente
-    private final JPanel buttonsPanel;                                                                                  //Panel für Atomare Komponenten
-    private final JScrollPane todoScrollPane;                                                                           //Initialisierung des scrollbaren Bereiches
-    private final JPanel todoPanel;                                                                                     //Panel, dass alle Todo Elemente enthält
+
+// Aufbau der grafischen Oberfläche (View in MVC)
+// Es wird die Schnittstelle ListDataListener implementiert, damit die Klasse TodoListView als Listener im DatenModell registrieren kann
+public final class TodoListView implements ListDataListener{
+    private final TodoList controller; // Referenz auf die zentrale Steuerklasse TodoList
+    private final TodoListModel model; // Referenz auf TodoListModel als Datenmodell
+    private final JPanel masterPanel; // Panel über den gesamten Inhalt der Oberfläche
+    private final JPanel buttonsPanel; // Panel für Eingabefelder in der Kopfzeile
+    private final JScrollPane todoScrollPane; // Scrollbarer Hauptbereich der To-do Liste
+    private final JPanel todoPanel; // Panel innherhalb des scrollbaren Hauptbereichs, hält später die einzelnen To-do Elemente
 
     public TodoListView(TodoList controller, TodoListModel model){
         this.controller = controller;
         this.model = model;
 
-        JFrame frame = new JFrame("TodoList");                                                                          //Erstellung der Top Level Komponente - Hauptfenster der Todo Liste
+        JFrame frame = new JFrame("TodoList"); // Erstellung der Top-Level-Komponente - Hauptfenster der To-do Liste
         frame.setSize(500, 500);
 
-        this.masterPanel = new JPanel();                                                                                //Erstellung des Hauptpanels und Border Layouts der Todo Liste
+        this.masterPanel = new JPanel(); // Erstellung des Hauptpanels und BorderLayouts der To-do Liste
         this.masterPanel.setLayout(new BorderLayout());
         frame.add(this.masterPanel);
-//Erstellung aller Eingabefelder und einer Schaltfläche innerhalb der Todo Liste
-        JTextField inputHeader = new JTextField("Titel");                                                               //Erstellung des Textfeldes 'Titel'
-        JTextField inputMessage = new JTextField("Aufgabe");                                                            //Erstellung des Textfeldes 'Aufgabe'
-        JButton inputSubmit = new JButton("Hinzufügen");                                                                //Erstellung des Buttons 'Hinzufügen'
-        inputSubmit.addActionListener((ActionEvent _) -> {                                                              // Registrierung des ActionListeners für den "Hinzufügen" Button
-            controller.addTodoElement(new TodoElement(inputHeader.getText(), inputMessage.getText()));                  // Als Reaktion wird ein neues Element mit den eingegeben Daten hinzugefügt
-            inputHeader.setText("Titel");                                                                               // Eingabefeld 'Titel' wird auf Platzhaltertext zurückgesetzt
-            inputMessage.setText("Aufgabe");                                                                            // Eingabefeld 'Aufgabe' wird auf Platzhaltertext zurückgesetzt
-        });
-//Initialisierung des Panels zur Eingabe eines neuen Todo Elementes
-        this.buttonsPanel = new JPanel();
-        this.buttonsPanel.setLayout(new BoxLayout(this.buttonsPanel, BoxLayout.LINE_AXIS));                             //Horizontale Anordnung des gesamten Eingabebereiches
-        this.buttonsPanel.add(inputHeader);                                                                             //Eingabefeld für die Überschrift des Todo Elementes
-        this.buttonsPanel.add(inputMessage);                                                                            //Eingabefeld für den Inhalt des Todo Elementes
-        this.buttonsPanel.add(inputSubmit);                                                                             //Schaltfläche zum hinzufügen des Todo Elementes
-        masterPanel.add(this.buttonsPanel, BorderLayout.PAGE_START);                                                    //Einfügen des Eiganbebereiches in den oberen Bereich der Todo Liste
-//Initilaisierung eines Panels zur Anzeige der Todo Elemente
-        this.todoPanel = new JPanel();
-        this.todoPanel.setLayout(new BoxLayout(this.todoPanel, BoxLayout.PAGE_AXIS));                                   //Todo Elemente werden vertikal angeordnet
-        this.todoScrollPane = new JScrollPane(todoPanel);                                                               //Hinzufügen eines vertikalen Scrollbalkens in die Ansicht der Todo Elemente
-        this.todoScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);                          //Einstellung damit der vertikale Scrollbalken dauerhaft sichtbar bleibt
-        this.todoScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);                       //Kein horizontaler Scrollblaken soll erstellt werden
-        masterPanel.add(this.todoScrollPane, BorderLayout.CENTER);                                                      //Positionieren des Ansichtsfeldes für Todo Elemente im Zentrum der Todo Liste
 
-        frame.setVisible(true);                                                                                         //Einstellung die die Todo Liste sichtbar macht
+        // Erstellung der Eingabefelder für neue To-do Elemente
+        JTextField inputHeader = new JTextField("Titel"); // Erstellung des Textfeldes 'Titel'
+        JTextField inputMessage = new JTextField("Aufgabe"); // Erstellung des Textfeldes 'Aufgabe'
+        JButton inputSubmit = new JButton("Hinzufügen"); // Erstellung des Buttons 'Hinzufügen'
+
+        // Registrierung des ActionListeners für den "Hinzufügen" Button
+        inputSubmit.addActionListener((ActionEvent _) -> {
+            controller.addTodoElement(new TodoElement(inputHeader.getText(), inputMessage.getText())); // Als Reaktion wird ein neues Element mit den eingegeben Daten hinzugefügt
+            inputHeader.setText("Titel"); // Eingabefeld 'Titel' wird auf Platzhaltertext zurückgesetzt
+            inputMessage.setText("Aufgabe"); // Eingabefeld 'Aufgabe' wird auf Platzhaltertext zurückgesetzt
+        });
+
+        // Die Eingabefelder werden in einen separaten Panel organisiert und dem Hauptpanel als Kopfzeile hinzugefügt
+        this.buttonsPanel = new JPanel();
+        this.buttonsPanel.setLayout(new BoxLayout(this.buttonsPanel, BoxLayout.LINE_AXIS)); // Horizontale Anordnung des gesamten Eingabebereiches
+        this.buttonsPanel.add(inputHeader);
+        this.buttonsPanel.add(inputMessage);
+        this.buttonsPanel.add(inputSubmit);
+        masterPanel.add(this.buttonsPanel, BorderLayout.PAGE_START);
+
+        // Initilaisierung des scrollbaren Panels zur Anzeige der To-do Elemente
+        this.todoPanel = new JPanel();
+        this.todoPanel.setLayout(new BoxLayout(this.todoPanel, BoxLayout.PAGE_AXIS)); // To-do Elemente werden vertikal angeordnet
+        this.todoScrollPane = new JScrollPane(todoPanel); // Das Panel wird einer Scrollpane untergeordnet, damit der Bereich bei Überlauf vertikales Scrollen ermöglicht
+        this.todoScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); // Einstellung damit der vertikale Scrollbalken dauerhaft sichtbar bleibt
+        this.todoScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // Kein horizontaler Scrollbalken soll erstellt werden
+        masterPanel.add(this.todoScrollPane, BorderLayout.CENTER); // Positionieren des Ansichtsfeldes für To-do Elemente im Zentrum der To-do Liste
+
+        frame.setVisible(true); // Hauptfenster wird sichtbar gestellt
     }
-    //Aufräumen der Todo Liste und neu hinzfügen aller aktuellen Todo Elemente
+
+    // Alle To-do Elemente werden entfernt und die gesamte To-do Liste neu eingelesen
     public void update() {
         this.todoPanel.removeAll();
-//Iterator zum feststellen der aktuellen Todo Elemente, damit diese wieder hinzugefügt werden können
         model.iterator().forEachRemaining(element -> {
             this.addElement(element);
         });
 
-        this.todoPanel.revalidate();
+        this.todoPanel.revalidate(); // Neuvalidierung und Neuzeichnen der GUI
         this.todoPanel.repaint();
     }
-    //Methode die aufgerufen wird, wenn Elemente zu der Todo Liste hinzugefügt werden
+
+    // Wird als Rückrufmethode aufgerufen, wenn neue Elemente der To-do Liste hinzugefügt wurden
     @Override
     public void intervalAdded(ListDataEvent e) {
         System.out.println("intervalAdded: " + e.getIndex0() + " - "  + e.getIndex1());
-        update();                                                                                                       //Aktualisierung der gesamten Liste
+        update(); // Neuaufbau der gesamten Liste                                                                                                       
     }
-    //Methode die aufgerufen wird, wenn Elemente aus der Todo Liste entfernt  werden
+
+    // Wird als Rückrufmethode aufgerufen, wenn Elemente aus der To-do Liste entfernt wurden
     @Override
     public void intervalRemoved(ListDataEvent e) {
         System.out.println("intervalRemoved: " + e.getIndex0() + " - "  + e.getIndex1());
-        update();
+        update(); // Neuaufbau der gesamten Liste  
     }
-    //Methode die aufgerufen wird, wenn Inhalte innerhalb der Todo Liste geändert werden
+
+    // Wird als Rückrufmethode aufgerufen, wenn sich die To-do Liste in einer anderen Weise verändert hat
     @Override
     public void contentsChanged(ListDataEvent e) {
         System.out.println("contentsChanged: " + e.getIndex0() + " - "  + e.getIndex1());
-        update();
+        update(); // Neuaufbau der gesamten Liste  
     }
-    //Methode die aufgerufen wird wenn ein Todo Element zu der Todo Liste hinzugefügt werden soll
+
+    // Methode zum Aufbau und Hinzufügen eines neuen To-do Elements als grafische Einheit
     void addElement(TodoElement element){
+        // Panel mit Überschrift des To-do Elements als Randbeschreibung
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
         panel.setBorder(new TitledBorder(new LineBorder(Color.GRAY), element.getHeader()));
         panel.setAlignmentY(0.0f);
         panel.setAlignmentX(0.0f);
-//Textbereich für den Inhalt des Todo Elementes wird Initialisiert
+
+        // Textbereich für die Aufgabenbeschreibung des To-do Elementes
         JTextArea message = new JTextArea(element.getMessage());
         message.setEditable(false);
         message.setFocusable(false);
         message.setLineWrap(true);
-//Initialisierung des Scrollbalkens innerhalb des Todo Elementes
+
+        // Initialisierung des Scrollbereichs bei Überlauf innerhalb des To-do Elementes
         JScrollPane scrollPane = new JScrollPane(message);
         scrollPane.setAlignmentY(0.0f);
         scrollPane.setAlignmentX(0.0f);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setMaximumSize(new Dimension(400, 100));
         panel.add(scrollPane);
-//Initialiserung der Schaltflächen: 'erledigt', 'hoch', 'runter'
+
+        // Initialiserung der Schaltflächen: 'erledigt', 'hoch', 'runter' am rechten Rand des Elementes
         JButton done = new JButton("erledigt");
         JButton up = new JButton("🠉");
         JButton down = new JButton("🠋");
-//Initialisieren von Aktionen für die Schaltflächen innerhalb der Todo Liste
+
+        // Initialisieren von Aktionen für die Schaltflächen
         done.addActionListener((ActionEvent _) -> {
-            this.controller.removeTodoElement(element);                                                                 //Hinzufügen der Aktion 'Entferne das Todo Element aus der Liste'
+            this.controller.removeTodoElement(element); // Hinzufügen der Aktion 'Entferne das To-do Element aus der Liste'
         });
 
         up.addActionListener((ActionEvent _) -> {
-            this.controller.moveElementUp(element);                                                                     //Hinzufügen der Aktion 'Bewege das Todo Element in der Liste nach oben'
+            this.controller.moveElementUp(element); // Hinzufügen der Aktion 'Bewege das To-do Element in der Liste nach oben'
         });
 
         down.addActionListener((ActionEvent _) -> {
-            this.controller.moveElementDown(element);                                                                   //Hinzufügen der Aktion 'Bewege das Todo Element in der Liste nach unten'
+            this.controller.moveElementDown(element); // Hinzufügen der Aktion 'Bewege das To-do Element in der Liste nach unten'
         });
-//Erstellen eines Panels für die Schaltflächen
+
+        // Erstellen eines Panels für die Schaltflächen
         JPanel buttons = new JPanel();
         buttons.setAlignmentY(0.0f);
-        buttons.setLayout(new BoxLayout(buttons, BoxLayout.PAGE_AXIS));                                                 //Vertikale Anordnung der Schaltflächen im Panel
-        buttons.add(done);                                                                                              //Hinzufügen der Schaltfläche 'erledigt'
-        buttons.add(up);                                                                                                //Hinzufügen der Schaltfläche 'hoch'
-        buttons.add(down);                                                                                              //Hinzufügen der Schaltfläche 'runter'
+        buttons.setLayout(new BoxLayout(buttons, BoxLayout.PAGE_AXIS)); // Vertikale Anordnung der Schaltflächen im Panel
+        buttons.add(done); // Hinzufügen der Schaltfläche 'erledigt'
+        buttons.add(up); // Hinzufügen der Schaltfläche 'hoch'
+        buttons.add(down); // Hinzufügen der Schaltfläche 'runter'
 
-        panel.add(buttons);                                                                                             //Hinzufügen des Schaltflächen Panels in ein Todo Element Panel
+        panel.add(buttons); // Hinzufügen des Schaltflächen-Panels in das übergeordnete Panel
 
-        this.todoPanel.add(panel);                                                                                      //Vollständige Einbindung des gesamten Todo Elemente Panels in das Haupt Panel
+        this.todoPanel.add(panel); // Vollständige Einbindung des gesamten To-do Elemente Panels in das Hauptpanel aller Elemente
     }
 }
